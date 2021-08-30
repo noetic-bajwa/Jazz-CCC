7348f38a64af08c2182657ff5d2439f42a83d3ff
Bajwa <anus@noeticworld.com>
2021-08-30 16:06:59 +0500

    For Deployment

--- src/app/dashboard/report/report.component.ts
+++ src/app/dashboard/report/report.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { OperatorStatsDataService } from '../Services/operator-stats-data.service';
+import { Inject }  from '@angular/core';
+import { DOCUMENT } from '@angular/common'; 
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-report',
@@ -11,19 +14,20 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
-  msg:string = "Searched Record Will be Displayed Below";
+  msg:string = "Searched Records Will be Displayed Below";
   data:any={};
   StartingDate:any;
   EndingDate:any;
   maxDate: Date;
   currentPage:number;
-  constructor(private dataService:OperatorStatsDataService, private datePipe: DatePipe,private router: Router,private pageTitle:Title) { 
+  constructor(private dataService:OperatorStatsDataService, private datePipe: DatePipe,private router: Router,private pageTitle:Title,@Inject(DOCUMENT) document) { 
     this.pageTitle.setTitle('GameNow | Operator Stats');
     this.maxDate = new Date();
 
   }
 
   ngOnInit(): void {
+    
   }
 
   isEmpty(){
@@ -31,31 +35,94 @@ export class ReportComponent implements OnInit {
   }
 
   pageChanged(event: PageChangedEvent,fromDate:any,toDate:any): void {
-    this.dataService.getData(event.page-1,this.datePipe.transform(fromDate,'yyyyMMdd'),this.datePipe.transform(toDate,'yyyyMMdd')).subscribe(data=>{
-    this.data=data;
-      
-     },
-     err=>{
-      
-     }) 
+    if(fromDate == '' && toDate == ''){
+      this.dataService.getData(event.page-1,'','').subscribe(data=>{
+        this.data=data;
+          
+         },
+         err=>{
+          
+         }) 
 
+    };
+    if(fromDate != '' && toDate != ''){
+      this.dataService.getData(event.page-1,this.datePipe.transform(fromDate,'yyyyMMdd'),this.datePipe.transform(toDate,'yyyyMMdd')).subscribe(data=>{
+        this.data=data;
+          
+         },
+         err=>{
+          
+         }) 
 
+    }
+    
   }
+
+
   onClickCheckRecord(fromDate:any,toDate:any){
-    if(fromDate == '' ){ 
-       this.msg = "Please Select the Date Range";
+    
+    //if Both 'From Date' and 'To Date' fields are empty, then it will display all Data of Operator Stats
+    if(fromDate == '' && toDate == ''){
+    this.dataService.getData('','','').subscribe(data=>{
+    this.data=data;},
+    err=>{
+     
+    });
+    return;
+   };
+
+   //if 'From Date' is empty then it will display error for 3 sec and exit the function
+    if(fromDate == '' && toDate != ''){ 
+      this.data='';
+       this.msg = "Please Select 'From' Date";
+       document.getElementById("fromDate").focus();
       setTimeout(() => {
-        this.msg = "Searched Record Will be Displayed Below";
+        this.msg = "Searched Records Will be Displayed Below";
       }, 3000);
       return;
     };
+
+    //if 'To Date' is empty then it will display error for 3 sec and exit the function
+    if(toDate == '' && fromDate != ''){ 
+      this.data='';
+      this.msg = "Please Select 'To' Date";
+      document.getElementById("toDate").focus();
+     setTimeout(() => {
+       this.msg = "Searched Records Will be Displayed Below";
+     }, 3000);
+     return;
+   };
+
+   //if From Date is greater than To Date then it will display error for 3 sec and exit the function
+    if(fromDate > toDate  ){ 
+      this.data='';
+      this.msg = "Date Range is not Correct";
+     setTimeout(() => {
+       this.msg = "Searched Records Will be Displayed Below";
+     }, 3000);
+     this.StartingDate = '';
+     this.EndingDate = '';
+     return;
+   };
+
+   //If there is error then the function would exit in the Above condition (If/else) statments
+   //Only the valid input field will bypass contional statments the reach this section of Function
+   // 1st Argument is 0 which means 1st page
+
     this.dataService.getData(0,this.datePipe.transform(fromDate,'yyyyMMdd'),this.datePipe.transform(toDate,'yyyyMMdd')).subscribe(data=>{
     this.data=data;
+    
       
      },
      err=>{
       
      }) 
     
+  }
+
+
+  onClickReset(){
+    this.StartingDate = '';
+    this.EndingDate = '';
   }
 }
