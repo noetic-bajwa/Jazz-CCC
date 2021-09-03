83fb8c596bc1cf3f871d3c4d5477e971fa19931b
Bajwa <anus@noeticworld.com>
2021-09-03 11:14:04 +0500

    Yasin

--- src/app/dashboard/affiliate-report/affiliate-report.component.ts
+++ src/app/dashboard/affiliate-report/affiliate-report.component.ts
@@ -45,7 +45,9 @@ export class AffiliateReportComponent implements OnInit {
     //      }) 
 
     // };
+
     if(fromDate != '' && toDate != ''){
+      console.log("PAge Changed call hua hai")
       this.dataService.getData(this.datePipe.transform(fromDate,'yyyyMMdd'),this.datePipe.transform(toDate,'yyyyMMdd'),this.selectedValue,event.page-1).subscribe(data=>{
         this.data=data;
           
@@ -62,83 +64,82 @@ export class AffiliateReportComponent implements OnInit {
   onClickCheckRecord(fromDate:any,toDate:any){
     
     //if 'From Date' and 'To Date' and 'Affiliate Select' fields are empty, then it will display Error
-    if(fromDate == '' && toDate == '' && this.selectedValue == 0){
-      this.data='';
-       this.msg = "Please Select Date Range and Select Affiliate";
-       document.getElementById("fromDate").focus();
-      setTimeout(() => {
-        this.msg = "Searched Records Will be Displayed Below";
-      }, 3000);
-      return;
-   };
+          if(fromDate == '' && toDate == '' && this.selectedValue == 0){
+            this.data='';
+            this.msg = "Please Select Date Range and Select Affiliate";
+            document.getElementById("fromDate").focus();
+            setTimeout(() => {
+              this.msg = "Searched Records Will be Displayed Below";
+            }, 3000);
+            return;
+        };
 
    //if 'From Date' and 'To Date' are empty but 'Affiliate Select' is not Empty, then it will display error 
-    if(fromDate == '' && toDate == '' && this.selectedValue != 0 ){ 
-      this.data='';
-       this.msg = "Please Select Date Range";
-       document.getElementById("fromDate").focus();
-      setTimeout(() => {
-        this.msg = "Searched Records Will be Displayed Below";
-      }, 3000);
-      return;
-    };
+          if(fromDate == '' && toDate == '' && this.selectedValue != 0 ){ 
+            this.data='';
+            this.msg = "Please Select Date Range";
+            document.getElementById("fromDate").focus();
+            setTimeout(() => {
+              this.msg = "Searched Records Will be Displayed Below";
+            }, 3000);
+            return;
+          };
 
     //if 'From Date' and 'To Date' are not empty but 'Affiliate Select' is  Empty, then it will display error 
-    if(fromDate != '' && toDate != '' && this.selectedValue == 0 ){ 
-      this.data='';
-       this.msg = "Please Select Affiliate";
-       document.getElementById("fromDate").focus();
-      setTimeout(() => {
-        this.msg = "Searched Records Will be Displayed Below";
-      }, 3000);
-      return;
-    };
+          if(fromDate != '' && toDate != '' && this.selectedValue == 0 ){ 
+            this.data='';
+            this.msg = "Please Select Affiliate";
+            setTimeout(() => {
+              this.msg = "Searched Records Will be Displayed Below";
+            }, 3000);
+            return;
+          };
 
     //if 'From Date' is empty and 'To Date' and 'Affiliate Select'is not empty then it will display error
-    if(toDate != '' && fromDate == '' && this.selectedValue != 0){ 
-      this.data='';
-      this.msg = "Please Select 'From' Date";
-      document.getElementById("toDate").focus();
-     setTimeout(() => {
-       this.msg = "Searched Records Will be Displayed Below";
-     }, 3000);
-     return;
-   };
+          if(toDate != '' && fromDate == '' && this.selectedValue != 0){ 
+            this.data='';
+            this.msg = "Please Select 'From' Date";
+            document.getElementById("toDate").focus();
+          setTimeout(() => {
+            this.msg = "Searched Records Will be Displayed Below";
+          }, 3000);
+          return;
+        };
 
    //if 'To Date' is empty and 'From Date' and 'Affiliate Select'is not empty then it will display error
-   if(toDate == '' && fromDate != '' && this.selectedValue != 0){ 
-    this.data='';
-    this.msg = "Please Select 'To' Date";
-    document.getElementById("toDate").focus();
-   setTimeout(() => {
-     this.msg = "Searched Records Will be Displayed Below";
-   }, 3000);
-   return;
- };
+          if(toDate == '' && fromDate != '' && this.selectedValue != 0){ 
+            this.data='';
+            this.msg = "Please Select 'To' Date";
+            document.getElementById("toDate").focus();
+          setTimeout(() => {
+            this.msg = "Searched Records Will be Displayed Below";
+          }, 3000);
+          return;
+        };
 
    //if From Date is greater than To Date then it will display error for 3 sec and exit the function
-    if(fromDate > toDate  && this.selectedValue == 0){ 
-      this.data='';
-      this.msg = "Date Range is not Correct and Select Affilate";
-     setTimeout(() => {
-       this.msg = "Searched Records Will be Displayed Below";
-     }, 3000);
-     this.StartingDate = '';
-     this.EndingDate = '';
-     return;
-   };
+          if(fromDate > toDate  && this.selectedValue == 0){ 
+            this.data='';
+            this.msg = "Date Range is not Correct and Select Affilate";
+          setTimeout(() => {
+            this.msg = "Searched Records Will be Displayed Below";
+          }, 3000);
+          this.StartingDate = '';
+          this.EndingDate = '';
+          return;
+        };
 
    //if From Date is greater than To Date then it will display error for 3 sec and exit the function
-   if(fromDate > toDate  && this.selectedValue != 0){ 
-    this.data='';
-    this.msg = "Date Range is not Correct";
-   setTimeout(() => {
-     this.msg = "Searched Records Will be Displayed Below";
-   }, 3000);
-   this.StartingDate = '';
-   this.EndingDate = '';
-   return;
- };
+        if(fromDate > toDate  && this.selectedValue != 0){ 
+          this.data='';
+          this.msg = "Date Range is not Correct";
+        setTimeout(() => {
+          this.msg = "Searched Records Will be Displayed Below";
+        }, 3000);
+        this.StartingDate = '';
+        this.EndingDate = '';
+        return;
+      };
 
  
 
@@ -146,24 +147,26 @@ export class AffiliateReportComponent implements OnInit {
    //Only the valid input field will bypass contional statments the reach this section of Function
    // 1st Argument is 0 which means 1st page
    
-    this.dataService.getData(this.datePipe.transform(fromDate,'yyyyMMdd'),this.datePipe.transform(toDate,'yyyyMMdd'),this.selectedValue,0).subscribe(data=>{
-    
-      this.data=data;
-    
-    console.log(data['total pages'])
-    
+      this.dataService.getData(this.datePipe.transform(fromDate,'yyyyMMdd'),this.datePipe.transform(toDate,'yyyyMMdd'),this.selectedValue,0).subscribe(data=>{
       
-     },
-     err=>{
+        this.data=data;
       
-     }) 
+      console.log(data['total pages'])
+      
+        
+      },
+      err=>{
+        
+      }) 
     
   }
 
 
   onChangeSelect(){
     if(this.data != '' || this.data != {}){
-      this.data = ''
+      this.data = '';
+      this.currentPage=0;
+
     }
   }
 
